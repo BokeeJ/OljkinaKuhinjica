7f750fc2ec293a41f27ee68703df1bd57c7cceb7
BokeeJ <bokeejusthard@gmail.com>
2025-06-11 23:04:09 +0200

    startinggg

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ function App() {
   return (
     <div>
       <h1
-        className="bg-pink-400">CAO RADIM</h1>
+        className="bg-pink-400">Cao Oljkaa! Ovde mozes pratiti sve sta radim i kad radim...sportski pozdrav od Zeca
+      </h1>
     </div>
   )
 }
